e46ee325e50a5c476ff804513dd629627b8f9d5c
ppresto <ppresto@hashicorp.com>
2019-06-25 15:21:48 -0700

    using local credentials

--- awscli/setEnv.sh
+++ awscli/setEnv.sh
@@ -3,22 +3,29 @@
 DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Source in AWS credentials (AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION)
 #ppresto admin
-echo "sourcing ${DIRECTORY}/.credentials"
- source ${DIRECTORY}/.credentials
+
+### replaced with default .aws/credentials
+#echo "sourcing ${DIRECTORY}/.credentials"
+#source ${DIRECTORY}/.credentials
+
 
 echo "Loading functions: ( aws , startawscli )"
 # Create aws CLI function/alias that uses my awscli container to execute commands
 aws() {
   docker run \
-  --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY} \
-  --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-  --env AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} \
   -v $PWD:/data \
+  -v $HOME/.aws/:/root/.aws \
   --rm ppresto/awscli aws "$@"
 }
 
 # Start container for dev.
 startawscli() {
+  docker run -it \
+  -v $PWD:/data \
+  -v $HOME/.aws/:/root/.aws \
+  --rm ppresto/awscli bash
+}
+startawscli_orig() {
   docker run -it \
   --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY} \
   --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
